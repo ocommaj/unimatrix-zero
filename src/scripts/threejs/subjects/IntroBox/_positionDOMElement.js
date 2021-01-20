bd3db1ca046edafaaa1d87cba7451d6846ef8b93
ocommaj <ocommaj@gmail.com>
2021-01-20 15:06:16 -0600

    update mobile ul yOffset

--- src/scripts/threejs/subjects/IntroBox/_positionDOMElement.js
+++ src/scripts/threejs/subjects/IntroBox/_positionDOMElement.js
@@ -22,7 +22,7 @@ export default function positionDOMElement(inMeshGroup, camera) {
   tempCenterVector.project(camera)
 
   const offsetXfactor = device.type === 'desktop' ? .45 : .225;
-  const offsetYfactor = device.type === 'desktop' ? .75 : .75; //.625
+  const offsetYfactor = device.type === 'desktop' ? .75 : .625;
   const offset = {
     x: (elemWidth * offsetXfactor),
     y: (elemHeight * offsetYfactor)
