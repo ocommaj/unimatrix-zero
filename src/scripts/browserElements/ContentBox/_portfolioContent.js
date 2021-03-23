b78fab133777eee87410d4a2beb39611d79a1272
ocommaj <ocommaj@gmail.com>
2021-03-23 10:52:20 -0500

    initial add of meta:og

--- src/scripts/browserElements/ContentBox/_portfolioContent.js
+++ src/scripts/browserElements/ContentBox/_portfolioContent.js
@@ -9,6 +9,19 @@ const CONTENT_ITEMS = [
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
   },
   {
+    projectID: 'xavierTech',
+    displayURL: 'Xavier Tech',
+    projectURL: 'https://www.xaviermicronesia.org/xbot',
+    previewType: 'img',
+    loadSrc: (element) => {
+      import(/*webpackMode: "lazy" */
+      '../../../assets/img/xbot_preview.gif')
+        .then(src => { element.src = src.default })
+    },
+    title: 'Xavier Tech Platform',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+  },
+  {
     projectID: 'xavierHome',
     displayURL: 'Xavier High School, FSM',
     projectURL: 'https://www.xaviermicronesia.org',
