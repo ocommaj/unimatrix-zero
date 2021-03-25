2bf3662f6c438691168a6991334b72f7f516fad5
ocommaj <ocommaj@gmail.com>
2021-03-25 14:34:50 -0500

    extend camera pos for iPhone8/SE

--- src/scripts/browserElements/_DeviceConfig.js
+++ src/scripts/browserElements/_DeviceConfig.js
@@ -41,8 +41,14 @@ function defaultCameraPos(deviceType) {
       config.yPos = -1.25;
       config.zPos = 16.75;
       break;
-    case '8':
+    case '8/SE':
+      config.yPos = -1.15;
+      config.zPos = 16.25;
+      break;
     case '8Plus':
+      config.yPos = -1.2;
+      config.zPos = 16.25;
+      break
     case 'XR/11':
     case 'X/XS/11Pro/12Mini':
     case 'XSMax/11ProMax':
@@ -91,41 +97,23 @@ function iPhoneModelDetect() {
   }
   const iPhone = true;
   if (width/height === 375/667 && dpr === 2) {
-    return {
-      iPhone,
-      model: '8'
-    }
+    return { iPhone, model: '8/SE' }
   }
   if (width/height === 414/736 && dpr === 3) {
-    return {
-      iPhone,
-      model: '8Plus'
-    }
+    return { iPhone, model: '8Plus' }
   }
   if (width/height === 375/812 && dpr === 3) {
-    return {
-      iPhone,
-      model: 'X/XS/11Pro/12Mini'
-    }
+    return { iPhone, model: 'X/XS/11Pro/12Mini' }
   }
   if (width/height === 414/896) {
-    return {
-      iPhone,
-      model: dpr === 2 ? 'XR/11' : 'XSMax/11ProMax'
-    }
+    return { iPhone, model: dpr === 2 ? 'XR/11' : 'XSMax/11ProMax' }
   }
 
   if (width/height === 390/844 && dpr === 3) {
-    return {
-      iPhone,
-      model: '12/12Pro'
-    }
+    return { iPhone, model: '12/12Pro' }
   }
   if (width/height === 428/926 && dpr === 3) {
-    return {
-      iPhone,
-      model: '12ProMax'
-    }
+    return { iPhone, model: '12ProMax' }
   }
   else {
     return {
