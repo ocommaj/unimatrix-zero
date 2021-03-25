eccafe39e71052fb2e3068cc3b9be50117c976b7
ocommaj <ocommaj@gmail.com>
2021-03-25 16:35:14 -0500

    add icon & link to github button

--- src/scripts/browserElements/_NavButtons.js
+++ src/scripts/browserElements/_NavButtons.js
@@ -11,7 +11,12 @@ const BUTTON_ITEMS = [
   },
   {
     id: 'github',
-    displayLabel: 'GitHub'
+    displayLabel: 'GitHub',
+    fontAwesomeIcon: 'fa-external-link-alt',
+    aTag: {
+      url: 'https://github.com/ocommaj',
+      target: 'blank'
+    }
   },
   {
     id: 'contact',
@@ -36,8 +41,16 @@ function NavButtons() {
     button.classList.add('navButton');
     button.dataset.sectionKey = item.id;
     button.innerHTML = item.displayLabel;
-    button.addEventListener('click', switchContent);
 
+    if (item.fontAwesomeIcon) {
+      button.appendChild( fontAwesomeIcon(item.fontAwesomeIcon) )
+    }
+
+    if (item.aTag) {
+      button.appendChild( linkButton(item.aTag) )
+    }
+
+    button.addEventListener('click', switchContent);
     buttons.appendChild(button)
   })
 
@@ -48,4 +61,18 @@ function NavButtons() {
   function reveal() {
     wrapper.classList.toggle('invisible')
   }
+}
+
+function linkButton(link) {
+  const element = document.createElement('a');
+  element.href = link.url;
+  element.target = link.target;
+  return element;
+}
+
+function fontAwesomeIcon(className) {
+  const element = document.createElement('i');
+  element.classList.add('fas')
+  element.classList.add(className)
+  return element;
 }
